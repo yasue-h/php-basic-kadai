b4c5209c6859ebf50ba42592ff43e2693fa0f2ef
yasue-h <153531235+yasue-h@users.noreply.github.com>
2024-02-10 00:37:04 +0900

    kadai_011アップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_011</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 連想配列
+        $personal_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+         
+        // 連想配列$personal_dataの値を1つずつ順番に出力する
+        foreach ($personal_data as $key => $value) {
+        echo "{$key}:{$value}<br>";
+            
+        }
+        ?>
+    </p>
+
+</body>
+
+</html>
+
+         
